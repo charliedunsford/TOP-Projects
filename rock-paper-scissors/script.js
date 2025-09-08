02bab8e41e1fb687f2e65dad9ef86537306954a8
Charlie Dunsford <184438218+charliedunsford@users.noreply.github.com>
2025-09-08 07:57:54 -0700

    feat(rock-paper-scissors): add logic for player choices

--- rock-paper-scissors/script.js
+++ rock-paper-scissors/script.js
@@ -1,4 +1,17 @@
+let humanScore = 0;
+let computerScore = 0;
+
 function getComputerChoice() {
 	let random = Math.ceil(Math.random() * 3);
-	return random == 3 ? "rock" : (random == 2 ? "paper" : "scissors");
+	return random == 3 ? "ROCK" : (random == 2 ? "PAPER" : "SCISSORS");
 }
+
+function getHumanChoice() {
+	return prompt("Rock, paper, or scissors?");
+}
+
+function playRound(humanChoice, computerChoice) {
+	console.log("Human: " + humanChoice);
+	console.log("Computer: " + computerChoice);
+}
+	
