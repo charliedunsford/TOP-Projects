cdb9ecc2b71bf0f6d2a8d4b9e924dbebc2557cd5
charliedunsford <184438218+charliedunsford@users.noreply.github.com>
2025-09-08 10:49:22 -0700

    feat(rock-paper-scissors): complete game logic

--- rock-paper-scissors/script.js
+++ rock-paper-scissors/script.js
@@ -1,6 +1,3 @@
-let humanScore = 0;
-let computerScore = 0;
-
 function getComputerChoice() {
 	let random = Math.ceil(Math.random() * 3);
 	return random == 3 ? "ROCK" : (random == 2 ? "PAPER" : "SCISSORS");
@@ -10,8 +7,27 @@ function getHumanChoice() {
 	return prompt("Rock, paper, or scissors?");
 }
 
-function playRound(humanChoice, computerChoice) {
-	console.log("Human: " + humanChoice);
-	console.log("Computer: " + computerChoice);
-}
+function playGame() {
+	let humanScore = 0;
+	let computerScore = 0;
 	
+	while (humanScore < 5 && computerScore < 5) {
+		const humanChoice = getHumanChoice().toUpperCase();
+		const computerChoice = getComputerChoice();
+		playRound(humanChoice, computerChoice);
+	}
+
+	function playRound(humanChoice, computerChoice) {
+		console.log("Human: " + humanChoice);
+		console.log("Computer: " + computerChoice);
+		if (humanChoice == "ROCK" && computerChoice == "SCISSORS" || humanChoice == "PAPER" && computerChoice == "ROCK" || humanChoice == "SCISSORS" && computerChoice == "PAPER") {
+			console.log(`You win! ${humanChoice} beats ${computerChoice}.`);
+			humanScore++;
+		} else {
+			console.log(`You lose! ${computerChoice} beats ${humanChoice}.`);
+			computerScore++;
+		}
+	}
+
+	console.log((humanScore == 5 ? "Human" : "Computer") + " has won!");
+}
